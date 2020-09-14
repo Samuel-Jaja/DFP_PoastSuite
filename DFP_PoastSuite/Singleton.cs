c3743be6b2b6ef320ddd2b65345a504e57d6c06a
Samuel Jaja <samuel.jaja21@gmail.com>
2020-09-14 08:13:00 +0100

    Add project files.

---
+++ DFP_PoastSuite/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DFP_PoastSuite
+{
+    class Singleton
+    {
+        private static Singleton  instance = null;
+        private Singleton() { }
+
+        public static Singleton Instance
+        {
+            get 
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+
+        public void DoSomething() 
+        {
+            Console.WriteLine("Singleton class code has been called for SJ");
+            Console.ReadLine();
+        }
+
+
+    }
+}
